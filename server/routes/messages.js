48dcf4162fdaf88b9414e56f2e1903a1aaf4560d
ola357 <alliolaoluwa@gmail.com>
2019-03-09 13:01:08 +0100

    feat(user): get message route -fetches all recieved messages for the user [Delivers #164487450]

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -1,5 +1,13 @@
 import express from 'express';
 
 const router = express.Router();
+const messages = [
+  {
+    id: 1, createdOn: Date.now, subject: 'Hello Friend', parentMessageId: 1, status: 'read',
+  },
+];
 
+router.get('/', (req, res) => {
+  res.status(200).send({ status: 200, data: messages });
+});
 export default router;
