9abcf72fc85c656a0e5c9f97b5920357dc2fc012
ola357 <alliolaoluwa@gmail.com>
2019-03-19 20:47:21 +0100

    feat(developer): implements the get routes -GET recieved messages -GET unread messages -GET sent messages -Implement LFA feedback -[Delivers #164726994]

--- server/models/models.js
+++ server/models/models.js
@@ -1,30 +1,23 @@
 import db from './db';
 
-const models = () => {
-  /* db.query(
-    `CREATE TABLE IF NOT EXISTS users(
+const models = async () => {
+  const createUsersTable = `CREATE TABLE IF NOT EXIST users(
       id SERIAL PRIMARY KEY,
       firstname VARCHAR NOT NULL,
       lastname VARCHAR NOT NULL,
       email VARCHAR NOT NULL UNIQUE,
       password VARCHAR NOT NULL
-    )`,
-  ); */
-  db.query(`CREATE TABLE IF NOT EXISTS "messages"(
-    “id” SERIAL PRIMARY KEY,
-    “createdon” timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    “subject” VARCHAR NOT NULL,
-    “message” VARCHAR NOT NULL,
-    ”parentmessageid” INTEGER,
-    “status” VARCHAR NOT NULL,
-    “receiverid” INTEGER REFERENCES "users"(id) ON DELETE CASCADE,
-    “senderid” INTEGER REFERENCES "users"(id) ON DELETE CASCADE
-    )`).then(() => db.query(`CREATE TABLE IF NOT EXISTS "users"(
-      "id" SERIAL PRIMARY KEY,
-      "firstname" VARCHAR NOT NULL,
-      "lastname" VARCHAR NOT NULL,
-      "email" VARCHAR NOT NULL UNIQUE,
-      "password" VARCHAR NOT NULL
-    )`));
+      )`;
+  const createMessagesTable = `CREATE TABLE IF NOT EXISTS "messages"(
+    id SERIAL PRIMARY KEY,
+    createdon timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    subject VARCHAR NOT NULL,
+    message VARCHAR NOT NULL,
+    parentmessageid INTEGER,
+    status VARCHAR NOT NULL,
+    receiverid INTEGER REFERENCES "users"(id) ON DELETE CASCADE,
+    senderid INTEGER REFERENCES "users"(id) ON DELETE CASCADE
+    )`;
+  await db.query(`${createUsersTable} ${createMessagesTable}`);
 };
 export default models;
