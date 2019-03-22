83b991c7014c9c0f2118f3d5432492522c5ddf29
ola357 <alliolaoluwa@gmail.com>
2019-03-22 22:23:06 +0100

    feat(developer): middleware uses jwt that protects -routes now sends json error object -signup routes send error 409 -no more 400 - User signup twice

--- server/middleware/Authorise.js
+++ server/middleware/Authorise.js
@@ -6,7 +6,12 @@ dotenv.config();
 class Authorise {
   static protect(req, res, next) {
     const token = req.header('x-auth-token');
-    if (!token) return res.status(401).send('Access denied. No token');
+    if (!token) {
+      return res.status(401).send({
+        status: 401,
+        error: "Access denied, No token",
+      });
+    }
 
     try {
       const decoded = jwt.verify(token, process.env.jwtPrivateKey);
